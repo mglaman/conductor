413aa1edc9234941eaed4b272ef29a9ff584ceac
Matt Glaman <nmd.matt@gmail.com>
2016-12-01 21:06:06 -0600

    Adjust tests

--- test/mainWindowTest.js
+++ test/mainWindowTest.js
@@ -12,31 +12,42 @@ if (process.platform === 'win32') {
 // Path to your application
 const appPath = path.join(__dirname, '..');
 
+global.before(function () {
+	chai.should();
+	chai.use(chaiAsPromised)
+});
+
 const app = new Application({
 	path: electronPath,
 	args: [appPath]
 });
 
 describe('application launch', function () {
-	this.timeout(10000)
+	this.timeout(10000);
 
 	beforeEach(function () {
-		this.app = new Application({
-			path: electronPath,
-			args: [appPath]
-		});
-		return this.app.start()
+		return app.start().then(function () {
+			assert.equal(app.isRunning(), true);
+			chaiAsPromised.transferPromiseness = app.transferPromiseness;
+			return app;
+		})
 	});
 
 	afterEach(function () {
-		if (this.app && this.app.isRunning()) {
-			return this.app.stop()
+		if (app && app.isRunning()) {
+			return app.stop()
 		}
 	});
 
 	it('shows an initial window', function () {
-		return this.app.client.getWindowCount().then(function (count) {
-			assert.equal(count, 1)
-		})
+		return app.client.waitUntilWindowLoaded()
+			.browserWindow.focus()
+			.getWindowCount().should.eventually.equal(1)
+			.browserWindow.isMinimized().should.eventually.be.false
+			.browserWindow.isDevToolsOpened().should.eventually.be.false
+			.browserWindow.isVisible().should.eventually.be.true
+			.browserWindow.isFocused().should.eventually.be.true
+			.browserWindow.getBounds().should.eventually.have.property('width').and.be.above(0)
+			.browserWindow.getBounds().should.eventually.have.property('height').and.be.above(0)
 	})
 });
